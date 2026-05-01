905a1c01e2c9005db2d9524051e244264614015b
Phileas <55714518+phileas0408@users.noreply.github.com>
2026-05-01 04:16:00 +0200

    add back rpmfusion

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,6 +4,10 @@ set -ouex pipefail
 
 ### Install packages
 
+dnf5 -y install \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm && \
+        
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
