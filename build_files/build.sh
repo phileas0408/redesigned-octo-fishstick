b5248b130dc0a6915a2c9d6815fa42c30126c045
Phileas <55714518+phileas0408@users.noreply.github.com>
2026-08-16 02:51:01 +0200

    Add Unity Hub installation to build script
    Added installation steps for Unity Hub to the build script.

--- build_files/build.sh
+++ build_files/build.sh
@@ -29,6 +29,12 @@ dnf5 config-manager setopt vscode.enabled=0
 # install discord from official package
 dnf5 install -y "https://discord.com/api/download?platform=linux&format=rpm"
 
+# install unity hub latest
+# https://docs.unity.com/en-us/hub/install-hub-linux#install-the-hub-on-red-hat-enterprise-linux-(rhel)-centos-or-fedora
+sh -c 'echo -e "[unityhub]\nname=Unity Hub\nbaseurl=https://hub.unity3d.com/linux/repos/rpm/stable\nenabled=1\ngpgcheck=1\ngpgkey=https://hub.unity3d.com/linux/repos/rpm/stable/repodata/repomd.xml.key\nrepo_gpgcheck=1" > /etc/yum.repos.d/unityhub.repo'
+dnf check-update
+dnf install unityhub
+
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
